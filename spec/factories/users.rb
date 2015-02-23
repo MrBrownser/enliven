7f63a5e7f90102c7a26593ab65c648f8e7e41c58
MrBrownser <castany.adria@gmail.com>
2015-02-23 15:33:27 +0100

    First model tests created and passing (using factory girl and faker gems)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :user do
-    
+  	username	{ Faker::Internet.user_name }
+  	email		{ Faker::Internet.email }
+  	born		{ Faker::Date.between(70.years.ago, 16.years.ago) }
+  	# password	{ Faker::Internet.password(8) }
+  	password	"12345678"
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "Validations testing" do
+		it "Validates the presence of email" do
+			user = build(:user, email: nil)
+
+			expect(user.valid?).to be false
+			expect(user.errors[:email].present?).to be true
+		end
+		
+		it "Validates the presence of username" do
+			user = build(:user, username: nil)
+
+			expect(user.valid?).to be false
+			expect(user.errors[:username].present?).to be true
+		end
+		
+		it "Validates the presence of birthday" do
+			user = build(:user, born: nil)
+
+			expect(user.valid?).to be false
+			expect(user.errors[:born].present?).to be true
+		end
+	end
 end
